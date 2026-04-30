d90ccb29861b6c4e2c80fcbe189d25aebb25594b
Titas Vaidila <titasvaidila2006@gmail.com>
2026-04-30 12:17:33 +0100

    First attempt at CNN architecture

--- train.py
+++ train.py
@@ -4,6 +4,8 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 from torch.utils.data import Subset
 from torch.utils.data import DataLoader
+import torch.nn as nn
+import torch.nn.functional as F
 
 # Define the transformations for the training dataset
 train_transform = transforms.Compose([
@@ -50,3 +52,26 @@ val_dataset = Subset(full_val_source, val_idx)
 # Create DataLoaders for training and validation datasets
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
 val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+
+class BreedCNN(nn.Module):
+    def __init__(self, num_classes=37):
+        super(BreedCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2) # Efficient pooling layer to reduce size of data
+        self.fc = nn.Linear(32 * 56 * 56, num_classes)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.pool(x)
+
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.pool(x)
+
+        x = x.view(-1, 32 * 56 * 56)
+
+        x = self.fc(x)
+
+        return x
